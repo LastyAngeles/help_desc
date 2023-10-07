bf6bc88b7f15ade31f1215940c2a89825bd9b23a
Maxim Meshkov <maksimmeshkovru@gmail.com>
2023-10-08 00:36:17 +0300

    Add agent API;

---
+++ src/HelpDesc.Api/IAgentGrain.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using Orleans;
+
+namespace HelpDesc.Api;
+
+public interface IAgentGrain : IGrainWithStringKey
+{
+    Task<bool> AssignSession(string sessionId);
+}
---
+++ src/HelpDesc.Api/IAgentManagerGrain.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using HelpDesc.Api.Model;
+using Orleans;
+
+namespace HelpDesc.Api;
+
+public interface IAgentManagerGrain: IGrainWithStringKey
+{
+    Task<Agent> AssignAgent(string sessionId);
+}
