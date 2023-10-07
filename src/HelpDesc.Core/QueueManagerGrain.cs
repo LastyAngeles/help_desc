2e6d9dbde9326120c850d90739e05b2c1b35f35b
Maxim Meshkov <maksimmeshkovru@gmail.com>
2023-10-07 17:01:38 +0300

    Add core project (basics) as dedicated executable;

---
+++ src/HelpDesc.Core/QueueManagerGrain.cs
@@ -0,0 +1,20 @@
+﻿using HelpDesc.Api.Model;
+using HelpDesc.Api;
+using Orleans;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HelpDesc.Core;
+
+public class QueueManagerGrain : Grain, IQueueManagerGrain
+{
+    private readonly List<string> sessionIds = new();
+
+    // TODO: inject IAgentManager (Maxim Meshkov 2023-10-07)
+
+    public Task<SessionCreateResult> CreateSession()
+    {
+        // TODO: create window (Maxim Meshkov 2023-10-07)
+        return Task.FromResult(new SessionCreateResult("mainId", true));
+    }
+}
