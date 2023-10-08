614a1a98db6e602087df7d6b80f09f9d16d3174a
Maxim Meshkov <maksimmeshkovru@gmail.com>
2023-10-08 17:17:21 +0300

    Add blank API for sessionGrain;

---
+++ src/HelpDesc.Api/ISessionGrain.cs
@@ -0,0 +1,9 @@
+﻿using HelpDesc.Api.Model;
+using Orleans;
+
+namespace HelpDesc.Api;
+
+public interface ISessionGrain : IGrainWithStringKey
+{
+    public SessionStatus GetStatus();
+}
---
+++ src/HelpDesc.Core/SessionGrain.cs
@@ -0,0 +1,13 @@
+﻿using HelpDesc.Api;
+using HelpDesc.Api.Model;
+using Orleans;
+
+namespace HelpDesc.Core;
+
+public class SessionGrain : Grain, ISessionGrain
+{
+    public SessionStatus GetStatus()
+    {
+        throw new System.NotImplementedException();
+    }
+}
