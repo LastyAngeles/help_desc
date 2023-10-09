83f2d125881a4a6e886c09b39320e9aa35e191b7
Maxim Meshkov <maksimmeshkovru@gmail.com>
2023-10-09 23:51:37 +0300

    Add basic sessionQueueManager test;

---
+++ test/HelpDesc.Core.Test/SessionQueueManagerTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using HelpDesc.Api;
+using Orleans.TestingHost;
+using Xunit;
+
+namespace HelpDesc.Core.Test;
+
+[Collection(ClusterCollection.Name)]
+public class SessionQueueManagerTest
+{
+    public TestCluster cluster;
+
+    public SessionQueueManagerTest(ClusterFixture fixture)
+    {
+        cluster = fixture.Cluster;
+    }
+
+    [Fact]
+    public async Task BasicCreateSessionTest()
+    {
+        var queueManager = cluster.GrainFactory.GetGrain<IQueueManagerGrain>(0);
+
+        var response = await queueManager.CreateSession();
+
+        response.ExceptionMessage.Should().BeNull();
+        response.Success.Should().BeTrue();
+        response.Id.Should().NotBeNull();
+    }
+}
